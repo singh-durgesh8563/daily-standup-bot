59b7a300823d01261dd28c5af9f10ee6c8865ba1
Durgesh Ramraj Singh <singh.durgesh.rajput@gmail.com>
2025-07-28 18:40:33 +0530

    Fix: Add intents to bot client

--- bot.py
+++ bot.py
@@ -1,17 +1,21 @@
 import discord
 import os
 
+# Define intents
+intents = discord.Intents.default()
+intents.messages = True  # Enable message reading permissions
+
 # Get Discord token and thread ID from environment variables
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 THREAD_ID = os.getenv("THREAD_ID")
 
-# Set up the bot client
-client = discord.Client()
+# Set up the bot client with intents
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}')
-    # Get the channel (thread) to send the reminder
+    # Get the thread to send the reminder
     channel = client.get_channel(int(THREAD_ID))
     await channel.send("Reminder: Please submit your standup updates!")
 
